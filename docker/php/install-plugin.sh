304108430751e6b46f26d9a639d3f0d04a85a088
Rémi Verschelde <rverschelde@gmail.com>
2020-01-21 12:26:22 +0100

    Docker: Make install-plugin.sh set proper user permissions
    Otherwise we get new files owned by root:root instead of the
    user who actually uses the script.

--- docker/php/install-plugin.sh
+++ docker/php/install-plugin.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker exec -it godotengine-org--php /usr/local/bin/php artisan plugin:install "$1"
+USERID=${SUDO_UID}
+if [ -z "${USERID}" ]; then
+  USERID=${UID}
+fi
+
+sudo docker exec -it godotengine-org--php bash -c "/usr/local/bin/php artisan plugin:install \"$1\" && chown ${USERID}:${USERID} -R plugins/"
