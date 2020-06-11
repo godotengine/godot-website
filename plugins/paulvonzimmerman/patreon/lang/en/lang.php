852f382aca896d2cd3c9ffd801a5c6b621b57336
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-06-11 10:36:38 +0200

    Store Patreon plugin inside the repository
    This removes the need to initialize a (broken) submodule URL
    to get a local copy of the website running.
    
    The plugin was cloned from https://github.com/pcvonz/oc-patreongoalstatus-plugin
    (commit 5d685ddc9bbb522d668f7906185f24b1319da3b3).

---
+++ plugins/paulvonzimmerman/patreon/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'patreon',
+        'description' => 'Provides a component that displays the percentage complete of a patreon campaign',
+    ]
+];
