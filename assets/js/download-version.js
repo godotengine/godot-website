433bb67baa4471809be69a26936c612b98bb473a
Godot Organization <>
2024-06-06 07:38:13 +0000

    Deploying to published from @ godotengine/godot-website@a75602608367f7c61c14800a68d83bebc4d3c573 🚀

--- assets/js/download-version.js
+++ assets/js/download-version.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const downloadToggles = document.querySelectorAll('.preview-download-toggle');
-	downloadToggles.forEach((item) => {
-		const toggleLink = item.querySelector('h4');
-		toggleLink.addEventListener('click', (e) => {
-			if (item.classList.contains('active')) {
-				item.classList.remove('active');
-				toggleLink.textContent = 'Show all downloads';
-			} else {
-				item.classList.add('active');
-				toggleLink.textContent = 'Hide all downloads';
-			}
-		});
-	});
-});
+document.addEventListener('DOMContentLoaded',()=>{const a=document.querySelectorAll('.preview-download-toggle');a.forEach(a=>{const b=a.querySelector('h4');b.addEventListener('click',c=>{a.classList.contains('active')?(a.classList.remove('active'),b.textContent='Show all downloads'):(a.classList.add('active'),b.textContent='Hide all downloads')})})})
