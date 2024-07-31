dcd7d975e2f383faea4b4390829faf148c35b380
Adam Scott <ascott.ca@gmail.com>
2024-07-31 16:37:15 -0400

    Fix missing top contributors

--- assets/js/releases/4.3.mjs
+++ assets/js/releases/4.3.mjs
@@ -103,8 +103,11 @@ scales[0] = Math.floor(scales[1] / 2);
 
 for (const author of authors) {
 	const prs = Number(author.dataset.prs);
+	let continued = false;
 	for (let i = 0; i < scales.length; i++) {
+		continued = false;
 		if (prs >= scales[i]) {
+			continued = true;
 			continue;
 		}
 		if (i === 0) {
@@ -112,5 +115,8 @@ for (const author of authors) {
 		}
 		author.classList.add(`size-${i + 1}`);
 		break;
+	}
+	if (continued) {
+		author.classList.add(`size-${scales.length + 1}`);
 	}
 }
