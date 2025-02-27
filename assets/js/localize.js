bf79f99062348a8920c3f73a056470bb57673d71
Emi <coppolaemilio@protonmail.com>
2025-02-27 19:33:16 +0100

    Add localization plugin and initial `es` translation (#1011)
    * Add plugin and initial `es` translation
    
    * Add missing features to plugin
    
    * Translate home page and sponsors
    
    * Add language selector and widen nav
    
    * Fix mobile style and language selector
    
    * Add js helper file
    
    * Update header.css
    
    * translate footer

---
+++ assets/js/localize.js
@@ -0,0 +1,16 @@
+// Get the language label
+const languageMap = {
+	'en': 'English',
+	'es': 'Español',
+	'de': 'Deutsch',
+	'ja': '日本語',
+	'zh': '中文',
+	'pt': 'Português'
+};
+
+document.querySelectorAll('.localize-language-label').forEach(function(el) {
+	const lang = el.textContent;
+	if (languageMap[lang]) {
+		el.textContent = languageMap[lang];
+	}
+});
