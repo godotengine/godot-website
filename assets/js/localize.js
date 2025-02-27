247c319cd2123301d2c4df2c16d3ea3126c4b530
Hugo Locurcio <hugo.locurcio@hugo.pro>
2025-02-27 23:57:30 +0100

    Add French localization

--- assets/js/localize.js
+++ assets/js/localize.js
@@ -3,9 +3,10 @@ const languageMap = {
 	'en': 'English',
 	'es': 'Español',
 	'de': 'Deutsch',
+	'fr': 'Français',
 	'ja': '日本語',
 	'zh': '中文',
-	'pt': 'Português'
+	'pt': 'Português',
 };
 
 document.querySelectorAll('.localize-language-label').forEach(function(el) {
