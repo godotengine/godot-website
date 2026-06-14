96a58325b2e67d2c2278df15d2506c2bdc78676e
Tibo <goiffonthibaud@protonmail.com>
2026-06-14 19:03:28 +0200

    add: background gradients

---
+++ assets/js/releases/gradient_quad_mat.js
@@ -0,0 +1,29 @@
+import * as THREE from "../modules/three.module.min.js";
+
+export let MakeGradientMat = function(lightColor){
+    return new THREE.ShaderMaterial({
+        transparent: true,
+        uniforms: {
+            color: {
+                value: new THREE.Color(lightColor)
+            },
+        },
+        vertexShader: `
+            varying vec2 vUv;
+    
+            void main() {
+                vUv = uv;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+            }
+        `,
+        fragmentShader: `
+            uniform vec3 color;
+        
+            varying vec2 vUv;
+            
+            void main() {
+                gl_FragColor = vec4(color, (1.0 - length(vUv.xy - vec2(1.0))));
+            }
+        `,
+    });
+}
